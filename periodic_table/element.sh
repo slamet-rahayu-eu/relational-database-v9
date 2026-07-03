62e71966bb0957f4f0bd08698e1162116c279782
Slamet Rahahyu <slamet_rahayu93@student.esaunggul.ac.id>
2026-07-03 16:39:29 +0700

    add periodic table

---
+++ periodic_table/element.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+else
+  if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    ELEMENT_RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e INNER JOIN properties p USING(atomic_number) INNER JOIN types t USING(type_id) WHERE e.atomic_number = $1")
+  else
+    ELEMENT_RESULT=$($PSQL "SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius FROM elements e INNER JOIN properties p USING(atomic_number) INNER JOIN types t USING(type_id) WHERE e.symbol = '$1' OR e.name = '$1'")
+  fi
+
+  if [[ -z $ELEMENT_RESULT ]]
+  then
+    echo "I could not find that element in the database."
+  else
+    IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT <<< "$ELEMENT_RESULT"
+    echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+  fi
+fi
